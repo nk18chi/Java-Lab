7904414af8668e366ce4404640f7f0d71a542580
Naoki Mita <naoki.mita18@gmail.com>
2019-10-28 10:03:58 -0700

    Refactoring mini project

--- src/MiniProject/GameController.java
+++ src/MiniProject/GameController.java
@@ -8,6 +8,36 @@ import java.util.Scanner;
 
 public class GameController {
 
+  public static void main(String[] args) {
+    GameController game = new GameController();
+//    System.out.println(game.getAnswer());
+    while (game.getMissingCount() < game.REMAIN_COUNT ) {
+      Input i = new Input(game.getInputHistory());
+      game.addInputHistory(i.getInput());
+
+      Question q = new Question(game.getAnswer(), i.getInput(), game.getCurrentResult());
+      q.printResult();
+      if (!q.isMatch()) {
+        game.addInputWrongHistory(i.getInput());
+        game.addMissingCount();
+      }
+
+      if (q.isCorrect()) break;
+
+      game.printMissingCount();
+      game.setCurrentResult(q.getResult());
+      game.setInputHistory(game.getInputHistory());
+      game.setInputWrongHistory(game.getInputWrongHistory());
+    }
+
+    if (game.getMissingCount() == 10) {
+      game.printEndFailureStatement();
+    } else {
+      game.printEndSuccessStatement();
+    }
+  }
+
+
   public static final int REMAIN_COUNT = 10;
 
   private String answer;
@@ -103,11 +133,11 @@ public class GameController {
   }
 
   public void addInputHistory(String i) {
-    this.inputHistory.add(i);
+    inputHistory.add(i);
   }
 
   public void addInputWrongHistory(String i) {
-    this.inputWrongHistory.add(i);
+    inputWrongHistory.add(i);
   }
 
   public void printStartStatement() {
